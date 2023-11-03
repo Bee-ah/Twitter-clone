85c689b9869968118c9d22bb45724981bbb0ca39
Beatriz <beatrizleite_aragao@hotmail.com>
2023-11-02 21:20:58 -0300

    novos arquivos

--- social/views.py
+++ social/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render , redirect
 from django.contrib import messages
-from .models import Profile
+from .models import Profile , Message
+from .forms import MessageForm
 
 def home(request):
-    return render(request , 'home.html' , {})
+    if request.user.is_authenticated:
+        form = MessageForm(request.POST or None)
+        if request.method == "POST":
+            if form.is_valid():
+                mensagem = form.save(commit=False)
+                mensagem.user = request.user
+                mensagem.save()
+                messages.success(request , ("Your message was sent"))
+                return redirect('home')
+        mensagens = Message.objects.all().order_by("-created_at")
+        return render(request , 'home.html' , {"mensagens": mensagens , "form":form})
+    else:
+        mensagens = Message.objects.all().order_by("-created_at")
+        return render(request , 'home.html' , {"mensagens": mensagens})
 
 def profile_list(request):
     if request.user.is_authenticated:
@@ -16,17 +30,25 @@ def profile_list(request):
 def profile(request , pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id = pk) 
-
+        mensagens = Message.objects.filter(user_id = pk).order_by("-created_at") 
+        form = MessageForm(request.POST or None)
         if request.method == "POST":
-            current_user_profile = request.user.profile
-            action = request.POST['follow']
-            if action =="unfollow":
-                current_user_profile.follows.remove(profile)
-            elif action =="follow":
-                current_user_profile.follows.add(profile)
-            current_user_profile.save()
-
-        return render(request , 'profile.html' , {"profile":profile})  
+            if "follow" in request.POST:
+                current_user_profile = request.user.profile
+                action = request.POST['follow']
+                if action =="unfollow":
+                    current_user_profile.follows.remove(profile)
+                elif action =="follow":
+                    current_user_profile.follows.add(profile)
+                current_user_profile.save()
+            elif "form" in request.POST:
+                if form.is_valid():
+                    mensagem = form.save(commit=False)
+                    mensagem.user = request.user
+                    mensagem.save()
+                    messages.success(request , ("Your message was sent"))
+                    return redirect('profile' , pk=pk)
+        return render(request , 'profile.html' , {"profile":profile , "mensagens":mensagens , "form": form})  
     else:
         messages.success(request , ("You must be logged in to view this page"))
         return redirect('home') 
